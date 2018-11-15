8a0a72f89e007ea7fd5bae6b1fd56249892d4d7a
Aleksandr Lopatkin <aleksandr_lopatkin@epam.com>
2018-11-15 21:50:27 +0300

    Removed script for deployment

--- starter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -xe
-
-STACK_NAME="network"
-
-if [[ $(aws cloudformation describe-stacks --query 'Stacks[].StackName' --output text) == "${STACK_NAME}" ]]; then
-	aws cloudformation update-stack --stack-name "${STACK_NAME}" --template-body file://network.yaml --parameters file://parameters.json
-else
-	aws cloudformation create-stack --stack-name "${STACK_NAME}" --template-body file://network.yaml --parameters file://parameters.json
-fi
